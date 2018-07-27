6f82cb162e020e54b370291f2796a4ca1bc1d355
wbroberts <willbarronroberts@gmail.com>
2018-07-27 10:21:12 -0600

    refactored

--- validation/socialMediaValidation.js
+++ validation/socialMediaValidation.js
@@ -5,37 +5,12 @@ const isEmpty = require('./isEmpty');
 // Checks that the social media links are real URLs
 const socialMediaValidation = obj => {
   const errors = {};
-  const { facebook, linkedin, youtube, instagram, twitter } = obj;
-
-  if (facebook) {
-    if (!validator.isURL(facebook)) {
-      errors.facebook = 'Not a valid URL';
+  // Iterate over each key and check if it is a valid URL
+  Object.keys(obj).map(key => {
+    if (!validator.isURL(obj[key])) {
+      errors[key] = 'Not a valid URL';
     }
-  }
-
-  if (linkedin) {
-    if (!validator.isURL(linkedin)) {
-      errors.linkedin = 'Not a valid URL';
-    }
-  }
-
-  if (youtube) {
-    if (!validator.isURL(youtube)) {
-      errors.youtube = 'Not a valid URL';
-    }
-  }
-
-  if (instagram) {
-    if (!validator.isURL(instagram)) {
-      errors.instagram = 'Not a valid URL';
-    }
-  }
-
-  if (twitter) {
-    if (!validator.isURL(twitter)) {
-      errors.twitter = 'Not a valid URL';
-    }
-  }
+  });
 
   // Check if errors object is empty
   if (!isEmpty(errors)) {
