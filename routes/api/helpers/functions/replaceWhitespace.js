1730ece1f7997a46a84c6f854a86a16adf94d5b5
wbroberts <willbarronroberts@gmail.com>
2018-08-01 10:28:12 -0600

    refactored

--- routes/api/helpers/functions/replaceWhitespace.js
+++ routes/api/helpers/functions/replaceWhitespace.js
@@ -1,14 +1,11 @@
 // This is to ensure that the handles do not include whitespace.
 // Defaults to '-' for URL use.
 const replaceWhitespace = (str, replacement = '-') => {
-  str = str.toLowerCase().trim();
-
-  // Exit out of function early if no whitespace
-  if (!str.includes(' ')) return str;
   // Replace all instances of whitespace with replacement
-  str = str.replace(/\s/g, replacement);
-
-  return str;
+  return str
+    .trim()
+    .replace(/\s/g, replacement)
+    .toLowerCase();
 };
 
 module.exports = replaceWhitespace;
